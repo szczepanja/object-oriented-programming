78f0e18c2ab9574f53978dc6d7a1f0a53de109a4
a_szczepanik <a.szczepanik@adform.com>
2022-04-22 14:18:18 +0200

    Add method print and test for it

--- src/main/scala/Exercise2/Account.scala
+++ src/main/scala/Exercise2/Account.scala
@@ -1,5 +1,6 @@
 package Exercise2
 
 case class Account(balance: Int, number: Int) {
+  def print: String = s"""balance: $balance, number: $number"""
 
 }
--- src/test/scala/Exercise2/AccountSpec.scala
+++ src/test/scala/Exercise2/AccountSpec.scala
@@ -6,5 +6,13 @@ import org.scalatest.matchers._
 class AccountSpec extends AnyFlatSpec with should.Matchers {
 
   behavior of "Account"
+  val account1: Account = Account(10000, 1)
+  val account2: Account = Account(22000, 2)
+
+  "account.print" should "print a statement for an account" in {
+
+    account1.print shouldBe "balance: 10000, number: 1"
+    account2.print shouldBe "balance: 22000, number: 2"
+  }
 
 }
